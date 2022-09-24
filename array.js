a496f08250da3a92fe19c958294cf38da99a75d2
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-24 13:50:34 +0600

    object in array practiced

---
+++ array.js
@@ -0,0 +1,15 @@
+// const products = [
+//     {name: 'phone', price: 12000},
+//     {name: 'laptop', price: 34000}
+// ]
+
+// products[0];
+
+const products = [
+    '0' = 1,
+    '3' = 3,
+    '4' = 5
+]
+
+products[0];
+console.log(products);
